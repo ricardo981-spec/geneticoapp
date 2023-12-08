ded072ac9fe9a0dc770d17d47832bb3788ae5b5b
ricardo981-spec <ricardocoutinholima009@gmail.com>
2023-12-08 10:05:57 -0300

    asdasdasdadsadsa

---
+++ frontend/src/style/theme/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from '@mui/material/style'
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            main:'blue'
+        }
+    },
+    typography:{
+        fontFamily:+'Arial,sans-serif'
+    }
+})
+export default theme
