5087d7d3e02592033540e66c5d5260f0aed0a4dc
ricardo981-spec <ricardocoutinholima009@gmail.com>
2023-12-08 08:46:52 -0300

    qweqwe

--- src/index.js
+++ src/index.js
@@ -3,23 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import theme from '../src/style/theme/theme.js';
-import {BrowserRouter,Routes,} from "react-router-dom"
-import {ThemeProvider} from '@mui/material/style' 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
+  <BrowserRouter>
       <Routes>
-        <Routes path="/" element={<app/>}/>
-        <Routes path="/oi" element={<h1>oi</h1>}/>
+          <Route path="/" element={<App/>}/>
       </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-    <App />
+  </BrowserRouter>
   </React.StrictMode>
 );
+
 reportWebVitals();
